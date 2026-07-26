ad42dee5f8b366f25da571a49273f25752ff2d46
kacidabersatu <kacidabersatu@gmail.com>
2026-07-26 18:54:09 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,58 @@
-const CACHE_NAME = "kacida-bersatu-v3-final-fix";
-const ASSETS_TO_CACHE = [
-  "./",
-  "index.html",
-  "manifest.json"
+const CACHE_NAME = 'kacida-pwa-cache-v1';
+const urlsToCache = [
+  './',
+  'manifest.json'
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (e) => {
-  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)))));
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
+
+// EVENT BUKA LINK SAAT NOTIFIKASI DI HP DIKLIK PENGGUNA
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  
+  var targetUrl = (event.notification.data && event.notification.data.url) 
+    ? event.notification.data.url 
+    : 'https://sites.google.com/view/laporansementara';
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url === targetUrl && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(targetUrl);
+      }
+    })
+  );
 });
